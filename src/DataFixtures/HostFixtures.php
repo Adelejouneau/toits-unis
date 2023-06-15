2f129d86b41ec561c01eaa393fa45f9718a64d7f
NassimaSi-bachir <125278524+NassimaSi-bachir@users.noreply.github.com>
2023-06-15 12:47:59 +0200

    fixtchl

--- src/DataFixtures/HostFixtures.php
+++ src/DataFixtures/HostFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Host;
+use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class HostFixtures extends Fixture
-{
+{   
+    public const PARTICULIER='particulier';
+    public const ENTREPRISE='entreprise';
     public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    {   
+        $host = new Host("");
+        $host -> setEntreprise("");
+        $host -> setProfession("");
+        $host-> setlastName("Fari");
+        $host-> setFirstName("LYLY");
+        $host-> setEmail("");
+        $host-> setPhoneUser("");
+        $host-> setImageNameUser("");
+        $host-> setPassword("");
+        // $host -> setUpdateAt(new DateTimeImmutable);
+        $host->setGender("");
+        $manager->persist($host);
 
         $manager->flush();
     }
