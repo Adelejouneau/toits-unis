4063bd23ca7f0f8e2f6ba791239eba5ed8ad7fee
sidibe-bintou <sidib.bintou@gmail.com>
2023-06-15 12:35:06 +0200

    ckeditor

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('updatedAt')
-            ->add('descriptionComment')
+            ->add('descriptionComment',  CKEditorType::class)
         ;
     }
 
