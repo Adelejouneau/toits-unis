1b67327a7fd4c280e5594ee71ccc0fdd444f7616
Adèle Jouneau <121813874+Adelejouneau@users.noreply.github.com>
2023-06-20 11:01:15 +0200

    Update CommentCaMarcheController.php

--- src/Controller/CommentCaMarcheController.php
+++ src/Controller/CommentCaMarcheController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CommentCaMarcheController extends AbstractController
+class CommentCaMarcheHostController extends AbstractController
 {
-    #[Route('/comment/ca/marche', name: 'app_comment_ca_marche')]
+    #[Route('/comment/ca/marche/host', name: 'app_comment_ca_marche_host')]
     public function index(): Response
     {
-        return $this->render('comment_ca_marche/index.html.twig', [
-            'controller_name' => 'CommentCaMarcheController',
+        return $this->render('comment_ca_marche_host/index.html.twig', [
+            'controller_name' => 'CommentCaMarcheHostController',
         ]);
     }
 }
