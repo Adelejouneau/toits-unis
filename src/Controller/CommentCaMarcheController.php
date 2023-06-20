069c78779c4e2f8c321ffa4fde1fe5d8b88dfee1
NassimaSi-bachir <125278524+NassimaSi-bachir@users.noreply.github.com>
2023-06-20 17:33:50 +0200

    frontA

--- src/Controller/CommentCaMarcheController.php
+++ src/Controller/CommentCaMarcheController.php
@@ -8,13 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CommentCaMarcheHostController extends AbstractController
+class CommentCaMarcheController extends AbstractController
 {
-    #[Route('/comment/ca/marche/host', name: 'app_comment_ca_marche_host')]
+    #[Route('/comment/ca/marche', name: 'app_comment_ca_marche')]
     public function index(): Response
     {
-        return $this->render('comment_ca_marche_host/index.html.twig', [
-            'controller_name' => 'CommentCaMarcheHostController',
+        return $this->render('comment_ca_marche/index.html.twig', [
+            'controller_name' => 'CommentCaMarcheController',
         ]);
     }
 }
