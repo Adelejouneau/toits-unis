40f0f790b1539b13a4ed50894ee825a1e57c1646
Adèle Jouneau <121813874+Adelejouneau@users.noreply.github.com>
2023-06-21 00:16:34 +0200

    push modifs bintou

--- src/Controller/CommentCaMarcheController.php
+++ src/Controller/CommentCaMarcheController.php
@@ -1,18 +1,16 @@
 <?php
-
 namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\CommentCaMarcheController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CommentCaMarcheController extends AbstractController
 {
-    #[Route('/comment/ca/marche/', name: 'app_comment_ca_marche')]
+    #[Route('/comment/ca/marche', name: 'app_comment_ca_marche')]
     public function index(): Response
     {
         return $this->render('comment_ca_marche/index.html.twig', [
-            'controller_name' => 'CommentCaMarcheController',
+            'controller_name' => 'CommentCaMarcheHostController',
         ]);
     }
 }
