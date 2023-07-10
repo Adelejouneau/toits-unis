b737eaab52a2ea34ab60e0a6439a83734ace6bde
sidibe-bintou <sidib.bintou@gmail.com>
2023-07-11 01:53:45 +0400

    page show lodging

--- src/DataFixtures/LodgingFixtures.php
+++ src/DataFixtures/LodgingFixtures.php
@@ -27,112 +27,130 @@ class LodgingFixtures extends Fixture
         }
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description de l'appartement aménagé");
+        $lodging->setDescriptionLod("Description de l'appartement aménagé, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image1.jpg");
         $lodging->setTitleLod("Chambre aménagée");
         $lodging->setSlugLod("logement-1");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::AIN));
         $lodging->setCategory($this->getReference(CategoryFixtures::APPARTEMENT));
-        $lodging->setUser($user);
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 2");
+        $lodging->setDescriptionLod("Description du logement 2, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image2.jpg");
         $lodging->setTitleLod("Local entreprise");
         $lodging->setSlugLod("logement-2");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::HAUTES_ALPES));
         $lodging->setCategory($this->getReference(CategoryFixtures::APPARTEMENT));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 3");
+        $lodging->setDescriptionLod("Description du logement 3, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image3.jpg");
         $lodging->setTitleLod("Hôtel");
         $lodging->setSlugLod("logement-3");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::PARIS));
         $lodging->setCategory($this->getReference(CategoryFixtures::CHAMBRE_PARTAGEE));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 4");
+        $lodging->setDescriptionLod("Description du logement 4, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image4.jpg");
         $lodging->setTitleLod("Coin salon agréable");
         $lodging->setSlugLod("logement-4");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::YVELINES));
         $lodging->setCategory($this->getReference(CategoryFixtures::COIN_CANAPE));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 6");
+        $lodging->setDescriptionLod("Description du logement 6, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image5.jpg");
         $lodging->setTitleLod("Chambre disponible");
         $lodging->setSlugLod("chambre-partagee");
-        $this->addReference('lodging_5', $lodging);
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::HAUTS_DE_SEINE));
         $lodging->setCategory($this->getReference(CategoryFixtures::ENTREPRISE));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description de l'appartement aménagé");
+        $lodging->setDescriptionLod("Description de l'appartement aménagé, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image6.jpg");
         $lodging->setTitleLod("Chambre aménagée");
         $lodging->setSlugLod("logement-1");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::AIN));
         $lodging->setCategory($this->getReference(CategoryFixtures::APPARTEMENT));
-        $lodging->setUser($user);
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 2");
+        $lodging->setDescriptionLod("Description du logement 2, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image7.jpg");
         $lodging->setTitleLod("Local entreprise");
         $lodging->setSlugLod("logement-2");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::HAUTES_ALPES));
         $lodging->setCategory($this->getReference(CategoryFixtures::APPARTEMENT));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 3");
+        $lodging->setDescriptionLod("Description du logement 3, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image8.jpg");
         $lodging->setTitleLod("Hôtel");
         $lodging->setSlugLod("logement-3");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::PARIS));
         $lodging->setCategory($this->getReference(CategoryFixtures::CHAMBRE_PARTAGEE));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 4");
+        $lodging->setDescriptionLod("Description du logement 4, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image9.jpg");
         $lodging->setTitleLod("Coin salon agréable");
         $lodging->setSlugLod("logement-4");
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::YVELINES));
         $lodging->setCategory($this->getReference(CategoryFixtures::COIN_CANAPE));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
         $lodging = new Lodging();
-        $lodging->setDescriptionLod("Description du logement 6");
+        $lodging->setDescriptionLod("Description du logement 6, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed est in lacus maximus aliquet.
+        Phasellus vulputate enim sit amet nunc ultricies, a semper tortor congue. 
+        Vestibulum sodales dui non odio tristique, a suscipit lectus consequat.
+        Fusce lacinia leo non nisi sagittis auctor.");
         $lodging->setImageNameLod("image10.jpg");
         $lodging->setTitleLod("Chambre disponible");
         $lodging->setSlugLod("chambre-partagee");
-        $this->addReference('lodging_5', $lodging);
         $lodging->setDepartment($this->getReference(AADepartmentFixtures::HAUTS_DE_SEINE));
         $lodging->setCategory($this->getReference(CategoryFixtures::ENTREPRISE));
-        $lodging->setUser($user);
         // $lodging->addDate($this->getReference(DateFixtures::20/10/23));
         $manager->persist($lodging);
 
