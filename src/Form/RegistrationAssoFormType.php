b1ea1b0269bf732f92c710471a2ed38e4809d209
sidibe-bintou <sidib.bintou@gmail.com>
2023-07-15 12:03:40 +0400

    fusion asso et user + fleche page home

--- src/Form/RegistrationAssoFormType.php
+++ src/Form/RegistrationAssoFormType.php
@@ -2,11 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Department;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -19,31 +23,67 @@ class RegistrationAssoFormType extends AbstractType
     {
         $builder
 
-            ->add('lastName', TextType::class, [
-                'label'=>"Nom de famille",
+        ->add('email')
+        ->add('plainPassword' , PasswordType::class, [
+            'label'=>"Nouveau mot de passe",
+            'mapped' => false,
+            'required'=>false,
             ])
-            ->add('firstName', TextType::class, [
-                'label'=>"Prenom",
+        ->add('nameAsso', null, [
+            'label' => 'Nom',
             ])
-            ->add('phoneUser')
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'label'=>"J'accepte les conditions d'utilisation",
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Vous devez accepter les conditons d\'utilisation.',
-                    ]),
-                ],
+        ->add('immatriculationAsso', null, [
+            'label' => 'immatriculation',
             ])
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'options' => ['attr' => ['class' => 'password-field']],
+        ->add('websiteUrl', null, [
+            'label' => 'site web',
+            ])
+        ->remove('imageNameUser')
+        ->add('imageFile', FileType::class, [
+            'required' => false,
+            'label' => "Image de l'association",
+        ])
+        ->remove('updatedAt', DateTimeType::class, [
+            'widget' => 'single_text',
+            'data' => new \DateTimeImmutable(),
+        ])
+        ->add('descriptionAsso', CKEditorType::class)
+        ->remove('roles', ChoiceType::class, [
+            'choices' =>[
+            'utilisateur' => 'user',
+            'Asso' => 'asso',
+        ],
+                'label' => 'Rôles',
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmer le mot de passe'],
-                'invalid_message' => 'Les mots de passe ne correspondent pas',
-            ]);
+                'mapped' => false,
+                ])
+        ->add('phoneNumberAsso', null, [
+            'label' => 'numero de telephone',
+            ])
+        // ->add('department', EntityType::class, [
+        //     'class' => Department::class,
+        //     'choice_label' => 'department',
+        //     'multiple' => true,
+        //     'expanded' => true,
+        //     'label' => 'Département',
+        // ])
+        ->add('agreeTerms', CheckboxType::class, [
+            'label'=>"J'accepte les conditions d'utilisation",
+            'mapped' => false,
+            'constraints' => [
+                new IsTrue([
+                    'message' => 'Vous devez accepter les conditons d\'utilisation.',
+                ]),
+            ],
+        ])
+        ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'options' => ['attr' => ['class' => 'password-field']],
+            'required' => true,
+            'first_options'  => ['label' => 'Mot de passe'],
+            'second_options' => ['label' => 'Confirmer le mot de passe'],
+            'invalid_message' => 'Les mots de passe ne correspondent pas',
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
