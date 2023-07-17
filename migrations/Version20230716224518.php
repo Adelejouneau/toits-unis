1efa9147d2d949e7589358f381776e0c4e5eeb53
Danielle Tang <danielletang111@gmail.com>
2023-07-17 13:21:26 +0200

    Créer Mentions légales + footer responsive

--- migrations/Version20230716224518.php
+++ migrations/Version20230716224518.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230715221453 extends AbstractMigration
+final class Version20230716224518 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20230715221453 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE association CHANGE immatriculation_asso immatriculation_asso VARCHAR(5) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user CHANGE immatriculation_asso immatriculation_asso VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE image_name_asso image_name_asso VARCHAR(255) NOT NULL, CHANGE slug_asso slug_asso VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE association CHANGE immatriculation_asso immatriculation_asso INT NOT NULL');
-        $this->addSql('ALTER TABLE user CHANGE immatriculation_asso immatriculation_asso INT NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE image_name_asso image_name_asso VARCHAR(255) DEFAULT NULL, CHANGE slug_asso slug_asso VARCHAR(255) NOT NULL');
     }
 }
