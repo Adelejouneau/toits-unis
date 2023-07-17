cbf767a6b74a863983808df167788defc19f2287
Adèle Jouneau <121813874+Adelejouneau@users.noreply.github.com>
2023-07-17 20:37:22 +0200

    corectrion

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
