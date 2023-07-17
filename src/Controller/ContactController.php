2fc4f5547530cecd6ec99aed777c724c859c9b21
sidibe-bintou <sidib.bintou@gmail.com>
2023-07-18 00:27:03 +0400

    fin

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,26 +11,11 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
-
-        $email = (new TemplatedEmail())
-                ->from(new Address('mailer@toitsUnis.com', 'toitsUnis'))
-                ->to($host->getEmail())
-                    ->subject('Bienvenue dans le réseau ToitsUnis !')
-                    ->htmlTemplate('contact/confirmation_contact_email.html.twig');
-                    
-            $this->mailer->send($email);
-
-            // do anything else you need here, like send an email
-            $this->addFlash('success','Inscription réussie, validez votre compte via le mail reçu.');
-            return $this->redirectToRoute('app_home');
-        }
-        
-       
-
-        return $this->render('home/indext.html.twig', [
+        return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
-
-
-    
+    }
 }
+
+
+       
